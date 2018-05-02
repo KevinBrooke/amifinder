32bf460fa17e45ca6ebc21b4acb43b4d3812f389
Kevin Brooke <kbrooke@live.com.au>
2018-05-02 11:41:46 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # amifinder
-Powershell script to obtain AWS AMI's for a certain instance type in all regions (handy for CloudFormation)
+Powershell script to obtain the latest version AWS AMI's for a certain instance type in all regions (handy for CloudFormation)
+Outputs in YAML format, ready to copy and paste.
---
+++ amiFinder.ps1
@@ -0,0 +1,17 @@
+# The AMI name to filter on (default is Amazon Linux)
+$filter = "amzn-ami-hvm*"
+
+# Obtain a list of regions
+$regions = aws ec2 describe-regions --output text | %{ $_.split("`t")[2]; }
+
+# Start the YAML output
+write-host "Mappings:"
+write-host "  AMIRegionMap:"
+
+# Loop through the regions
+foreach ($region in $regions)
+{
+    $ami = aws ec2 describe-images --region $region --filters Name=name,Values=$filter --query 'sort_by(Images,&CreationDate)[-1].ImageId' --output text
+    write-host "    "$region":"
+    write-host "      AMI: ""$ami"""
+}
