4d3e0a14e5a02877ac2e965fa2e2d09cde1321fb
Kevin Brooke <kbrooke@live.com.au>
2018-05-09 09:32:13 +0100

    Added filter for ebs volume type

--- amiFinder.ps1
+++ amiFinder.ps1
@@ -1,5 +1,6 @@
 # The AMI name to filter on (default is Amazon Linux)
-$filter = "amzn-ami-hvm*"
+$nameFilter = "amzn-ami-hvm*"
+$rootDeviceFilter = "ebs"
 
 # Obtain a list of regions
 $regions = aws ec2 describe-regions --output text | ForEach-Object{ $_.split("`t")[2]; }
@@ -11,7 +12,7 @@ write-host "  AMIRegionMap:"
 # Loop through the regions
 foreach ($region in $regions)
 {
-    $ami = aws ec2 describe-images --region $region --filters Name=name,Values=$filter --query 'sort_by(Images,&CreationDate)[-1].ImageId' --output text
+    $ami = aws ec2 describe-images --region $region --filters "Name=name,Values=$nameFilter" "Name=root-device-type,Values=$rootDeviceFilter" --query 'sort_by(Images,&CreationDate)[-1].ImageId' --output text
     write-host "    "$region":"
     write-host "      AMI: ""$ami"""
 }
