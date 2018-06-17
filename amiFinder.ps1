5ba1c901fd48d324e138ea046e456967613ccd60
KevinBrooke <kbrooke@live.com.au>
2018-06-17 22:38:27 +0100

    Parameters

--- amiFinder.ps1
+++ amiFinder.ps1
@@ -1,6 +1,16 @@
-# The AMI name to filter on (default is Amazon Linux)
-$nameFilter = "amzn-ami-hvm*"
-$rootDeviceFilter = "ebs"
+param(
+    [string]$os,
+    [string]$hdd
+)
+
+if (!$os)
+{
+    $os = 'amzn-ami-hvm*'
+}
+if (!$hdd)
+{
+    $hdd = 'ebs'
+}
 
 # Obtain a list of regions
 $regions = aws ec2 describe-regions --output text | ForEach-Object{ $_.split("`t")[2]; }
@@ -12,7 +22,7 @@ write-host "  AMIRegionMap:"
 # Loop through the regions
 foreach ($region in $regions)
 {
-    $ami = aws ec2 describe-images --region $region --filters "Name=name,Values=$nameFilter" "Name=root-device-type,Values=$rootDeviceFilter" --query 'sort_by(Images,&CreationDate)[-1].ImageId' --output text
+    $ami = aws ec2 describe-images --region $region --filters "Name=name,Values=$os" "Name=root-device-type,Values=$hdd" --query 'sort_by(Images,&CreationDate)[-1].ImageId' --output text
     write-host "   "$region":"
     write-host "      AMI: ""$ami"""
 }
